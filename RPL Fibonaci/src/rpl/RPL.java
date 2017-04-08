e1d7dbd2d80504c5fc31320861a3d7b11dcfad92
Doni Riyanta <driyanta31@gmail.com>
2017-04-08 11:51:33 +0700

    Update RPL.java

--- RPL Fibonaci/src/rpl/RPL.java
+++ RPL Fibonaci/src/rpl/RPL.java
@@ -1,7 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+‘modified sum of fibonacci’
+‘modified sum of fibonacci’
  */
 package rpl;
 import java.util.Scanner;
