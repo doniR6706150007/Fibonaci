1850fb5b22549e3145a3c46da073ec57c8e6f2ef
Doni Riyanta <driyanta31@gmail.com>
2017-04-08 11:41:11 +0700

    Add files via upload

---
+++ RPL Fibonaci/src/rpl/RPL.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpl;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class RPL {
+     public RPL() {
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n, tian, i;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Berikan Batas Deret Fibbonaci = ");
+        n = input.nextInt();
+   
+           for(i=1; i<=n; i++)
+           {
+          
+        tian = countFibbo(i);
+        System.out.print(tian+" ");
+           } 
+    }
+    //Fungsi Rekusif nya
+    public static int countFibbo(int n){  
+        int chan;
+        if(n==0)
+            return 0;
+        if(n==1)
+            return(1); 
+        else
+            chan = countFibbo(n-1) + countFibbo(n-2);           
+        return chan;
+    }
+    
+}
